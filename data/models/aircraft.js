48917c1f5c1e62b36e8fea2fe72fa3ede54189d0
Mark Dean <markbdean1@gmail.com>
2018-06-06 18:17:51 -0700

    refactor variable and function names

--- data/models/aircraft.js
+++ data/models/aircraft.js
@@ -5,7 +5,7 @@ const Aircraft = connection.define('aircrafts', {
   name: Sequelize.STRING
 })
 
-Aircraft.listOfNames = () => {
+Aircraft.fetch = () => {
   var table = Aircraft.findAll()
 
   var listOfNames = table.map(row => {
