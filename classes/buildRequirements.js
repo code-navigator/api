96fe4d5d7081a7c8a1535ae5db5c842a3d5ebb12
Mark Dean <markbdean1@gmail.com>
2018-07-18 20:12:56 -0700

    refactor BuildRequirements class

--- classes/buildRequirements.js
+++ classes/buildRequirements.js
@@ -4,7 +4,7 @@ const specNode = require("./../data/models/specNode")
 class BuildRequirements {
   constructor (id) {
     this.id = id
-    this.node = {}
+    this.node = null
     this.order = 1
     this.parentId = id
     this.requirements = []
@@ -22,9 +22,7 @@ class BuildRequirements {
     )
   }
 
-  async getRequirements () {
-    this.node = await specNode.fetch(this.parentId)
-    this.node = this.node[0]
+  async getNodeRequirements () {
     let reqs = await requirement.fetch(this.parentId)
     if (reqs.length > 0) {
       this.addHeading()
@@ -34,12 +32,28 @@ class BuildRequirements {
         this.requirements.push(element)
       })
     }
+  }
+
+  async getNode () {
+    this.node = await specNode.fetch(this.parentId)
+    this.node = this.node[0]
+  }
+
+  async getRequirements () {
+    // Get node for current ID
+    await this.getNode()
+    // Get requirements for current node
+    await this.getNodeRequirements()
     
+    // Bubble up to the next higher parent
     this.parentId = this.node.parentId
     
-    if(this.parentId !== '0') {
-      return await this.getRequirements()
+    // If the parent is not equal to the root node (i.e., id =0) ...
+    if(this.parentId.trim() !== '0') {
+      // ...Do recursion
+      return this.getRequirements()
     } else {
+      // ...Otherwise return the requirements
       return this.requirements
     }
   }
--- config.js
+++ config.js
@@ -1,26 +1,9 @@
 var config = {
   //mssql connection settings (home)
-  //  visualDb: {
-  //   user: 'mark',
-  //   password: 'password',
-  //   host: 'localhost',
-  //   db: 'Cygnus'
-  // },
-  // fileboundDb: {
-  //   user: 'FBADMIN',
-  //   password: 'filebound',
-  //   host: 'DM01',
-  //   db: 'Filebound',
-  // },
-  // paths: {
-  //   filebound: '\\\\Dm01\\e$\\FILEBOUND REPOSITORY\\'
-  // }
-
-  // mssql connection settings (work)
-  visualDb: {
-    user: 'sa',
-    password: 'N@4u2C0k!',
-    host: 'SQ03',
+   visualDb: {
+    user: 'mark',
+    password: 'password',
+    host: 'localhost',
     db: 'Cygnus'
   },
   fileboundDb: {
@@ -32,6 +15,23 @@ var config = {
   paths: {
     filebound: '\\\\Dm01\\e$\\FILEBOUND REPOSITORY\\'
   }
+
+  // mssql connection settings (work)
+//   visualDb: {
+//     user: 'sa',
+//     password: 'N@4u2C0k!',
+//     host: 'SQ03',
+//     db: 'Cygnus'
+//   },
+//   fileboundDb: {
+//     user: 'FBADMIN',
+//     password: 'filebound',
+//     host: 'DM01',
+//     db: 'Filebound',
+//   },
+//   paths: {
+//     filebound: '\\\\Dm01\\e$\\FILEBOUND REPOSITORY\\'
+//   }
 }
 
 module.exports = config
